23570f9497b97ab14db0e3439ce090858f971849
Damien <damien@fusion.ucla.edu>
2015-07-20 20:21:09 -0700

    clean up old levee crap

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_levee-backend_session'
+Rails.application.config.session_store :cookie_store, key: '_tofuya_session'
