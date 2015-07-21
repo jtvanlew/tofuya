68a99619818ab544c0b84965809b9868d8e4ccbc
Jon Van Lew <jonvanlew@gmail.com>
2015-07-20 22:32:19 -0700

    completes menu in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Item.create([
   {
-  name: "Bbq Beef Rib With Salad",
-  price: "9.99",
+  name: "B.B.Q. Beef Ribs With Salad",
+  price: "10.99",
   tag: "salad"
   },
   {
@@ -15,133 +15,193 @@ Item.create([
   tag: "salad"
   },
   {
+  name: "Green Salad",
+  price: "8.99",
+  tag: "salad"
+  },
+  {
   name: "Galbi",
-  description: "B.b.q. beef ribs seasoned w/ special souce",
-  price: "14.99",
+  description: "B.B.Q. beef ribs marinated in a house special sauce",
+  price: "16.99",
   tag: "bbq"
   },
   {
   name: "Bulgogi",
-  description: "B.b.q. slices of beef marinated in house special sauce",
-  price: "13.99",
+  description: "B.B.Q. slices of beef marinated in house special sauce",
+  price: "14.99",
   tag: "bbq"
   },
   {
   name: "Pork Bulgogi",
-  description: "B.b.q. slices of pork marinated in house spicy sauce",
-  price: "13.99",
+  description: "B.B.Q. slices of pork marinated in house spicy sauce",
+  price: "14.99",
+  tag: "bbq"
+  },
+  {
+  name: "Chicken Bulgogi",
+  description: "B.B.Q. slices of chicken marinated in house spicy sauce",
+  price: "14.99",
   tag: "bbq"
   },
   {
   name: "Chicken Katsu",
-  price: "12.99",
+  description: "Chicken cutlet",
+  price: "11.99",
+  tag: "bbq"
+  },
+  {
+  name: "Pork Katsu",
+  description: "Pork cutlet",
+  price: "13.99",
   tag: "bbq"
   },
   {
   name: "Chicken Teriyaki",
-  price: "9.99",
+  description: "Grilled chicken topped with teriyaki sauce",
+  price: "10.99",
   tag: "bbq"
   },
   {
-  name: "Seafood soon tofu",
-  price: "8.99",
+  name: "Seafood",
+  description: "Clam, shrimp, oyster",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Seafood & Beef soon tofu",
-  price: "8.99",
+  name: "Seafood & Beef",
+  description: "Clam, shrimp, oyster, beef",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Oyster soon tofu",
-  price: "8.99",
+  name: "Oyster",
+  description: "Oyster",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Mushroom soon tofu",
-  price: "8.99",
+  name: "Mushroom",
+  description: "Mushroom, beef",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Pork soon tofu",
-  price: "8.99",
+  name: "Pork",
+  description: "Pork",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Clam soon tofu",
-  price: "8.99",
+  name: "Clam",
+  description: "Clam",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Beef soon tofu",
-  price: "8.99",
+  name: "Beef",
+  description: "Beef",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Dumpling soon tofu",
-  price: "8.99",
+  name: "Dumpling",
+  description: "Dumplings, beef",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Miso soon tofu",
-  price: "8.99",
+  name: "Denjang",
+  description: "Miso, seaweed, onion, mushroom, beef",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Chicken soon tofu",
-  price: "8.99",
+  name: "Chicken",
+  description: "White meat chicken",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Vegetable soon tofu",
-  price: "8.99",
+  name: "Vegetable",
+  description: "Broccoli, mushroom, squash, carrot, onion",
+  price: "9.99",
   tag: "tofu"
   },
   {
-  name: "Galbi Soon Tofu",
-  description: "B.b.q. beef ribs seasoned w/special souce + soft tofu stew",
+  name: "Galbi & Sundubu",
+  description: "B.B.Q. beef ribs seasoned with special sauce + soft tofu stew",
+  price: "17.99",
+  tag: "combo"
+  },
+  {
+  name: "Bulgogi & Sundubu",
+  description: "Thin slices of beef marinated in a soy sauce + soft tofu stew",
+  price: "15.99",
+  tag: "combo"
+  },
+  {
+  name: "Pork Bulgogi & Sundubu",
+  description: "Thin slices of pork marinated in spicy sauce + soft tofu stew",
+  price: "15.99",
+  tag: "combo"
+  },
+  {
+  name: "Chicken Bulgogi & Sundubu",
+  description: "Thin slices of chicken marinated in spicy sauce + soft tofu stew",
+  price: "15.99",
+  tag: "combo"
+  },
+  {
+  name: "Chicken Teriyaki & Sundubu",
+  description: "Thin slices of pork marinated in spicy sauce + soft tofu stew",
+  price: "14.99",
+  tag: "combo"
+  },
+  {
+  name: "Chicken Katsu & Sundubu",
+  description: "Chicken cutlet + soft tofu stew",
+  price: "14.99",
+  tag: "combo"
+  },
+  {
+  name: "Pork Katsu & Sundubu",
+  description: "Pork cutlet + soft tofu stew",
+  price: "15.99",
+  tag: "combo"
+  },
+  {
+  name: "Kimchi Dolsot Bibimbop & Sundubu",
+  description: "This meal is served in a piping hot earthenware pot consisting of rice, cooked vegetables, & kimchi mixed with red chili paste (gochujang) + soft tofu stew",
+  price: "14.99",
+  tag: "combo"
+  },
+  {
+  name: "Dolsot Bibimbap & Sundubu",
+  description: "This meal is served in a piping hot earthenware pot consisting of rice, cooked vegetables, & beef mixed with red chili paste (gochujang) + soft tofu stew",
   price: "13.99",
   tag: "combo"
   },
   {
-  name: "Bulgogi Soon Tofu",
-  description: "B.b.q. sliced of beef marinated in house special sauce + soft stew",
+  name: "Bibimbap & Sundubu",
+  description: "Rice topped with various cooked vegetables, beef, and a fried egg, served with red chili paste (gochujang) + soft tofu stew",
   price: "12.99",
   tag: "combo"
   },
   {
-  name: "Pork Bulgogi Soon Tofu",
-  description: "B.b.q. slices of pork marinated in house spicy sauce + soft tofu stew",
-  price: "12.99",
-  tag: "combo"
-  },
-  {
-  name: "Dolsot Bibimbap Soon Tofu",
-  description: "Rice topped w/vegetable, meal & red pepper paste inn hot pot + soft tofu stew",
-  price: "12.99",
-  tag: "combo"
-  },
-  {
-  name: "Bibimbap Soon Tofu",
-  description: "Rice topped w/vegetable, meat & red pepper paste + soft tofu stew",
-  price: "11.99",
-  tag: "combo"
-  },
-  {
-  name: "Udon California Roll",
-  description: "Japanese noodle + roll w/avocado, crab & cucumber inside",
-  price: "7.99",
+  name: "Udon & California Roll",
+  description: "Japanese noodle + roll with avocado, imitation crab meat & cucumber",
+  price: "9.99",
   tag: "combo"
   },
   {
   name: "Dolsot Bibimbop",
-  description: "Rice topped w/vegetable, meat & red pepper paste in hot pot",
+  description: "This meal is served in a piping hot earthenware pot consisting of rice, cooked vegetables, & beef mixed with red chili paste (gochujang). This item can switch to a veggie version by substituting tofu for beef.",
   price: "9.99",
   tag: "special"
   },
   {
   name: "Bibimbop",
-  description: "Rice topped w/vegetable, meat & red pepper paste",
+  description: "Rice topped with various cooked vegetables, beef, and a fried egg, served with red chili paste (gochujang). This item can switch to a veggie version by substituting tofu for beef.",
   price: "8.99",
   tag: "special"
   },
@@ -153,13 +213,19 @@ Item.create([
   },
   {
   name: "California Roll",
-  description: "Roll w/avocado, crab & cucumber inside",
+  description: "Roll with avocado, imitation crab meat, & cucumber",
   price: "4.99",
   tag: "special"
   },
   {
   name: "Kimchi Dolsot Bibimbop",
-  description: "Rice topped w/vegetable, meat & red pepper paste in hot pot",
+  description: "This meal is served in a piping hot earthenware pot consisting of rice, cooked vegetables, & kimchi mixed with red chili paste (gochujang)",
+  price: "10.99",
+  tag: "special"
+  },
+  {
+  name: "Lunch Box",
+  description: "4pcs California roll, chicken teriyaki, salad, spring roll, edamame",
   price: "10.99",
   tag: "special"
   }
