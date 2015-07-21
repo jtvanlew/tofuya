62f11ccb70a0ec717cd21da64f811e021ae52898
Damien <damien@fusion.ucla.edu>
2015-07-20 21:44:53 -0700

    DB seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,166 @@
-
-Tagline.create(
-  {text: "Tax Savings For The Modern You."},
-  {text: "Make Tax Time A Breeze. Save Time And Money."},
-  {text: "Levee Your Own Pocket Tax Accountant."},
-  {text: "Earn More Money. We’ll Handle The Taxes For You."},
-  {text: "Never Miss A Tax Deduction or Deadline, Ever."}
-)
-
-SubTagline.create(
-  {text: "Saving receipts then filing your own taxes is a thing of the past. Securely auto track your earnings and deductions and get your taxes done by our tax experts, all in one place, to save time and money."},
-  {text: "Swipe right your business expenses straight to your tax accounting team to organize and do your taxes for you."},
-  {text: "Conveniently tracks your earnings and deductions automatically in our platform which allows us to manage your tax filings throughout the year."},
-  {text: "A secure mobile app that automates financial tracking and leverages tax experts to capture every deduction you deserve, legally."},
-  {text: "All your earnings and deductions are securely tracked automatically in one place throughout the year and your quarterly and annual tax returns prepared by our tax experts for you."}
-  )
-
-Price.create(
-  {amount: 10},
-  {amount: 15},
-  {amount: 20},
-  {amount: 25},
-  )
+Item.create([
+  {
+  name: "Bbq Beef Rib With Salad",
+  price: "9.99",
+  tag: "salad"
+  },
+  {
+  name: "Tofu With Salad",
+  price: "7.99",
+  tag: "salad"
+  },
+  {
+  name: "Chicken Teriyaki With Salad",
+  price: "8.99",
+  tag: "salad"
+  },
+  {
+  name: "Galbi",
+  description: "B.b.q. beef ribs seasoned w/ special souce",
+  price: "14.99",
+  tag: "bbq"
+  },
+  {
+  name: "Bulgogi",
+  description: "B.b.q. slices of beef marinated in house special sauce",
+  price: "13.99",
+  tag: "bbq"
+  },
+  {
+  name: "Pork Bulgogi",
+  description: "B.b.q. slices of pork marinated in house spicy sauce",
+  price: "13.99",
+  tag: "bbq"
+  },
+  {
+  name: "Chicken Katsu",
+  price: "12.99",
+  tag: "bbq"
+  },
+  {
+  name: "Chicken Teriyaki",
+  price: "9.99",
+  tag: "bbq"
+  },
+  {
+  name: "Seafood soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Seafood & Beef soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Oyster soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Mushroom soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Pork soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Clam soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Beef soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Dumpling soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Miso soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Chicken soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Vegetable soon tofu",
+  price: "8.99",
+  tag: "tofu"
+  },
+  {
+  name: "Galbi Soon Tofu",
+  description: "B.b.q. beef ribs seasoned w/special souce + soft tofu stew",
+  price: "13.99",
+  tag: "combo"
+  },
+  {
+  name: "Bulgogi Soon Tofu",
+  description: "B.b.q. sliced of beef marinated in house special sauce + soft stew",
+  price: "12.99",
+  tag: "combo"
+  },
+  {
+  name: "Pork Bulgogi Soon Tofu",
+  description: "B.b.q. slices of pork marinated in house spicy sauce + soft tofu stew",
+  price: "12.99",
+  tag: "combo"
+  },
+  {
+  name: "Dolsot Bibimbap Soon Tofu",
+  description: "Rice topped w/vegetable, meal & red pepper paste inn hot pot + soft tofu stew",
+  price: "12.99",
+  tag: "combo"
+  },
+  {
+  name: "Bibimbap Soon Tofu",
+  description: "Rice topped w/vegetable, meat & red pepper paste + soft tofu stew",
+  price: "11.99",
+  tag: "combo"
+  },
+  {
+  name: "Udon California Roll",
+  description: "Japanese noodle + roll w/avocado, crab & cucumber inside",
+  price: "7.99",
+  tag: "combo"
+  },
+  {
+  name: "Dolsot Bibimbop",
+  description: "Rice topped w/vegetable, meat & red pepper paste in hot pot",
+  price: "9.99",
+  tag: "special"
+  },
+  {
+  name: "Bibimbop",
+  description: "Rice topped w/vegetable, meat & red pepper paste",
+  price: "8.99",
+  tag: "special"
+  },
+  {
+  name: "Udon",
+  description: "Japanese noodle",
+  price: "6.99",
+  tag: "special"
+  },
+  {
+  name: "California Roll",
+  description: "Roll w/avocado, crab & cucumber inside",
+  price: "4.99",
+  tag: "special"
+  },
+  {
+  name: "Kimchi Dolsot Bibimbop",
+  description: "Rice topped w/vegetable, meat & red pepper paste in hot pot",
+  price: "10.99",
+  tag: "special"
+  }
+])
