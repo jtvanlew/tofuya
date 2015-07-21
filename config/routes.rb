f48c3c6eb6c24d91e4ffcd2b24c74fff456ccbf6
Damien <damien@fusion.ucla.edu>
2015-07-20 20:24:35 -0700

    more clean up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#pre_launch'
+  root 'pages#index'
 
   post 'sign_up' => 'pages#sign_up'
 
