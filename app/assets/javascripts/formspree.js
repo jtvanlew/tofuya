23bb3ae283f12533bfe032026dd54fd5cf8a39ba
jtvanlew <jonvanlew@gmail.com>
2016-03-15 22:49:33 -0700

    adds validation to formspree form

--- app/assets/javascripts/formspree.js
+++ app/assets/javascripts/formspree.js
@@ -3,30 +3,35 @@
   $(document).ready(function() {
 
     var $contactForm = $('#contact-form');
+    $contactForm.validate();
     $contactForm.submit(function(e) {
-      e.preventDefault();
-      debugger;
-      $.ajax({
-        url: '//formspree.io/hello@tofuya.la',
-        method: 'POST',
-        data: $(this).serialize(),
-        dataType: 'json',
-        beforeSend: function() {
-          $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-        },
-        success: function(data) {
-          $contactForm.find('.alert--loading').hide();
-          $contactForm.find('.alert--success').hide();
-          $contactForm.find('.alert--error').hide();
-          $contactForm.append('<div class="alert alert--success">Message sent!</div>');
-        },
-        error: function(err) {
-          $contactForm.find('.alert--loading').hide();
-          $contactForm.find('.alert--success').hide();
-          $contactForm.find('.alert--error').hide();
-          $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-        }
-      });
+        if ($(this).valid())
+             {
+               e.preventDefault();
+               debugger;
+               $.ajax({
+                 url: '//formspree.io/hello@tofuya.com',
+                 method: 'POST',
+                 data: $(this).serialize(),
+                 dataType: 'json',
+                 beforeSend: function() {
+                   $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+                 },
+                 success: function(data) {
+                   $contactForm.find('.alert--loading').hide();
+                   $contactForm.find('.alert--success').hide();
+                   $contactForm.find('.alert--error').hide();
+                   $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+                 },
+                 error: function(err) {
+                   $contactForm.find('.alert--loading').hide();
+                   $contactForm.find('.alert--success').hide();
+                   $contactForm.find('.alert--error').hide();
+                   $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+                 }
+               });
+             }
+       return false;
     });
   });
 
